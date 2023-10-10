b34fd098270f02ea4c59d0e5932472f57c4e5694
lasting <lastingcoder@qq.com>
2023-10-10 23:56:43 +0800

    fix: now the program run time is the real run time, not the difference between the start and end time of the program

--- heating.sh
+++ heating.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 terminate_program() {
-    take=$(($(date +%s) - $start))
-
+    echo $time_count
     for pid in ${pid_list[@]}; do
         kill $pid &>/dev/null
     done
 
-    if [ $take -le 60 ]; then
-        printf "\ntake %s seconds" $take
-    elif [ $take -le 3600 ]; then
-        printf "\ntake %s minutes and %s seconds" $(($take / 60)) $(($take % 60))
+    if [ $time_count -le 60 ]; then
+        printf "\ntake %s seconds" $time_count
+    elif [ $time_count -le 3600 ]; then
+        printf "\ntake %s minutes and %s seconds" $(($time_count / 60)) $(($time_count % 60))
     else
-        printf "\ntake %s hours and %s minutes and %s seconds" $(($take / 3600)) $(($take % 3600 / 60)) $(($take % 60))
+        printf "\ntake %s hours and %s minutes and %s seconds" $(($time_count / 3600)) $(($time_count % 3600 / 60)) $(($time_count % 60))
     fi
     printf "\n"
 
@@ -20,11 +19,17 @@ terminate_program() {
 
 animation() {
     frames="/ - | \\"
+    count=0
     while kill -0 $$ &>/dev/null; do
         for f in $frames; do
+            let count++
             printf "heating %s" $f
-            sleep 0.4
+            sleep 0.5
             printf "\r"
+            if [ $((count % 2)) -eq 0 ]; then
+                let count=0
+                let time_count++
+            fi
         done
     done
 }
@@ -34,6 +39,7 @@ printf "how many threads do you want? "
 read thread_nums
 
 pid_list=()
+time_count=0
 
 for ((index = 0; index < $thread_nums; index += 1)); do
     while :; do
@@ -41,12 +47,10 @@ for ((index = 0; index < $thread_nums; index += 1)); do
         let a++
         let a--
     done &
-    pid_list[$index]=$!
+    pid_list+=($!)
 done
 
 trap terminate_program INT
-
-start=$(date +%s)
 
 printf "pid: %s\n" $$
 printf "ctrl + c to terminate\n"
